d1fc401e31e03f9f68c7d931d298d54b8337008d
Nicholas Chin <nchin.nc@gmail.com>
2015-08-12 16:55:35 +0100

    changed db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150722161156) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
